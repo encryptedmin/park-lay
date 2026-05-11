dd3c77334de74fb538f9c47dba7f0576d7dc204c
Dion Odessy Padua <digitalfortress20@gmail.com>
2026-05-12 01:16:25 +0800

    Harden Render static and media serving

--- parklaysuites/urls.py
+++ parklaysuites/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +11,11 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        path(
+            f"{settings.MEDIA_URL.lstrip('/')}<path:path>",
+            serve,
+            {'document_root': settings.MEDIA_ROOT},
+        ),
+    ]
